254d40ca75e7cbdc5128a33a48917ea98e1e9353
jdev01-del <huangjase94@gmail.com>
2025-08-28 10:03:02 -0400

    fix: update base path and rebuild

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HuronTrading_v0.1/',
+  base: '/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
